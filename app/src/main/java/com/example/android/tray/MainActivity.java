d158a123c742dedbbd3e0342700d82047da09061
EdOates84 <niteshmaahwer1@gmail.com>
2020-01-28 23:49:27 +0530

    Add Navbar

--- app/src/main/java/com/example/android/tray/MainActivity.java
+++ app/src/main/java/com/example/android/tray/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.android.tray;
 
+import androidx.activity.OnBackPressedCallback;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -22,7 +25,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Cgroup = findViewById(R.id.Cbtn);
         Jgroup = findViewById(R.id.Jbtn);
+        FirebaseAuth fAuth;
+        FirebaseAuth.AuthStateListener fAuthlistener;
+        FirebaseUser CurrentUser;
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+//        drawer = findViewById(R.id.drawer_layout);
         fAuth = FirebaseAuth.getInstance();
+        CurrentUser = fAuth.getCurrentUser();
+        fAuthlistener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (firebaseAuth.getCurrentUser() == null) {
+                    startActivity(new Intent(getApplicationContext(), Login1Activity.class));
+                }
+            }
+        };
 
 
         Cgroup.setOnClickListener(new View.OnClickListener() {
@@ -39,5 +58,21 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        fAuth.addAuthStateListener(fAuthlistener);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent setIntent = new Intent(Intent.ACTION_MAIN);
+        setIntent.addCategory(Intent.CATEGORY_HOME);
+        setIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(setIntent);
+        finish();
     }
 }
